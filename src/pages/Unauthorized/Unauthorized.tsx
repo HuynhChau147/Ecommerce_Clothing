899fe2899b19ed1f4d0e1d972394edb119e272be
HuynhChauTapCode <huynhchau8743@gmail.com>
2022-10-22 18:40:38 +0700

    admin product item design

--- src/pages/Unauthorized/Unauthorized.tsx
+++ src/pages/Unauthorized/Unauthorized.tsx
@@ -1,10 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/UI/Button/Button';
+import './Unauthorized.scss';
+
 const Unauthorized = () => {
+  const navigate = useNavigate();
     return (
-      <section>
+      <section className="unauthorized">
+      <div>
         <h1>Unauthorized</h1>
         <br />
         <p>You do not have access to the requested page.</p>
-        <button>Go back</button>
+        <br />
+        <Button
+          className="btn--outline btn--primary-light btn--shadow"
+          onClick={() => navigate(-1)}
+        >
+          Go back
+        </Button>
+      </div>
       </section>
     );
   };
