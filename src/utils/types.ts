d9ce2d34a0a168a8dc510790779cbad5b6fb5c82
Nguyen Thi Ngoc Diem <nguyenthingocdiem211@gmail.com>
2022-12-04 11:46:41 +0700

    update status order and css admin order

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -96,13 +96,14 @@ export type SignUp = {
     password: string;
     name: string;
     passwordConfirm: string;
-  };
-  
+};
+
 // enum: ['Receive order', 'Confirm', 'Shipped', 'Cancelled', 'Return'],
 export enum StatusOrder {
     'Receive order' = 'Tiếp nhận đơn hàng',
     Confirm = 'Xác nhận đơn hàng',
-    Shipped = 'Đang vận chuyển',
+    Shipping = 'Đang vận chuyển',
+    Success = 'Giao hàng thành công',
     Cancelled = 'Hủy đơn hàng',
     Return = 'Đổi trả',
-  }
+}
